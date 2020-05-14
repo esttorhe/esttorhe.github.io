0800e3ccaaf6d1210adaea256af641665c255e03
Esteban Torres <me@estebantorr.es>
2020-05-14 14:48:41 +0200

    Removes gatsby stuff

--- src/templates/page.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default class AboutMe extends React.Component {
-    render() {
-        return <div
-          dangerouslySetInnerHTML={{ __html: this.props.pathContext.html }}>
-          </div>;
-    }
-}
